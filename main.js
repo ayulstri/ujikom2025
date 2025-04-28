dc0751b62b474ed83ab09db9ac3c3b36106270d3
ayulstri <ayulstri175@gmail.com>
2025-04-28 09:29:12 +0700

    update pembaruan

--- main.js
+++ main.js
@@ -43,3 +43,30 @@ export async function ambildaftartugas() {
   return hasil;
 }
 
+export async function tambahtugas(tugas, status, prioritas, tanggal) {
+  try {
+    const dokRef = await addDoc(collection(db, 'senin'), {
+      tugas: tugas,
+      status: status,
+      prioritas: prioritas,
+      tanggal: tanggal,
+    });
+    console.log('berhasil menembah tugas ' + dokRef.id);
+  } catch (e) {
+    console.log('gagal menambah tugas ' + e);
+  }
+}
+
+export async function hapustugas(docId) {
+  await deleteDoc(doc(db, "senin", docId));
+}
+
+export async function ubahtugas(docId, tugas, status, prioritas, tanggal) {
+  await updateDoc(doc(db, "senin", docId), {
+    tugas: tugas,
+    status: status,
+    prioritas: prioritas,
+    tanggal: tanggal,
+  });
+}
+
